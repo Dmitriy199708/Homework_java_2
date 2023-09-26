b7e49c5370b9e835f7927fffa8d0d568b6b724ea
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2023-09-26 23:30:36 +0300

    add homework

---
+++ src/main/java/org/example/Less.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Less {
+        public static void main(String[] args) {
+            int amountSpentOnTicket = 25899;
+            int bonus = 20;
+
+
+            int totalNumberOfBonuses =  amountSpentOnTicket / bonus;
+
+            System.out.println("Итоговое кол-во бонусов " + totalNumberOfBonuses);
+
+        }
+    }
+
